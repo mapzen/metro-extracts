2f963d19e8dba89d91a2be7289df9b79e97ea287
Michal Migurski <mike@teczno.com>
2016-06-24 16:09:56 -0700

    Scrubbed away remaining Github bits, replaced with Mapzen bits

--- App/oauth.py
+++ App/oauth.py
@@ -15,15 +15,19 @@ from flask import (
     )
 
 blueprint = Blueprint('OAuth', __name__, template_folder='templates/oauth')
-hardcoded_auth = ('mapzen', 'kMUjnU53')
+hardcoded_auth = ('mapzen', environ.get('TESTING_PASSWORD'))
+
+mapzen_token_url = 'https://mapzen.com/oauth/token'
+mapzen_authorize_url = 'https://mapzen.com/oauth/authorize'
+mapzen_currdev_url = 'https://mapzen.com/developers/oauth_api/current_developer'
 
 def apply_oauth_blueprint(app):
     '''
     '''
     app.register_blueprint(blueprint)
     app.secret_key = environ.get('FLASK_SECRET_KEY')
-    app.config['GITHUB_CLIENT_ID'] = environ.get('GITHUB_CLIENT_ID')
-    app.config['GITHUB_CLIENT_SECRET'] = environ.get('GITHUB_CLIENT_SECRET')
+    app.config['MAPZEN_APP_ID'] = environ.get('MAPZEN_APP_ID')
+    app.config['MAPZEN_APP_SECRET'] = environ.get('MAPZEN_APP_SECRET')
 
 def errors_logged(route_function):
     '''
@@ -46,9 +50,9 @@ def check_authentication(untouched_route):
     '''
     @wraps(untouched_route)
     def wrapper(*args, **kwargs):
-        ''' Prompt user to authenticate with password or Github if necessary.
+        ''' Prompt user to authenticate with password or Mapzen if necessary.
         '''
-        if current_app.config['GITHUB_CLIENT_ID'] is None:
+        if current_app.config['MAPZEN_APP_ID'] is None:
             auth = request.authorization
             if not auth or (auth.username, auth.password) != hardcoded_auth:
                 return Response(
@@ -64,7 +68,7 @@ def check_authentication(untouched_route):
             if access_token is None or user_id is None:
                 return make_401_response()
 
-            resp = get('https://mapzen.com/developers/oauth_api/current_developer',
+            resp = get(mapzen_currdev_url,
                        headers={'Authorization': 'Bearer {}'.format(access_token)})
             
             if resp.status_code in range(400, 499):
@@ -75,17 +79,17 @@ def check_authentication(untouched_route):
     return wrapper
 
 def make_401_response():
-    ''' Create an HTTP 401 Not Authorized response to trigger Github OAuth.
+    ''' Create an HTTP 401 Not Authorized response to trigger Mapzen OAuth.
     
-        Start by redirecting the user to Github OAuth authorization page:
-        http://developer.github.com/v3/oauth/#redirect-users-to-request-github-access
+        Start by redirecting the user to Mapzen OAuth authorization page:
+        https://github.com/mapzen/wiki/wiki/mapzen.com-OAuth
     '''
     state_id = str(uuid4())
     session['states'] = [dict(redirect=request.url, created=time())]
 
-    args = dict(href='https://mapzen.com/oauth/authorize')
+    args = dict(href=mapzen_authorize_url)
     args.update(redirect_uri=urljoin(request.url, '/oauth/callback'))
-    args.update(client_id=current_app.config['GITHUB_CLIENT_ID'])
+    args.update(client_id=current_app.config['MAPZEN_APP_ID'])
 
     return make_response(render_template('error-authenticate.html', **args), 401)
 
@@ -125,9 +129,9 @@ def hello():
 @blueprint.route('/oauth/callback')
 @errors_logged
 def get_oauth_callback():
-    ''' Handle Github's OAuth callback after a user authorizes.
+    ''' Handle Mapzen's OAuth callback after a user authorizes.
     
-        http://developer.github.com/v3/oauth/#github-redirects-back-to-your-site
+        https://github.com/mapzen/wiki/wiki/mapzen.com-OAuth
     '''
     if 'error' in request.args:
         return render_template('error-oauth.html', reason="you didn't authorize access to your account.")
@@ -144,18 +148,18 @@ def get_oauth_callback():
     
     #
     # Exchange the temporary code for an access token:
-    # http://developer.github.com/v3/oauth/#parameters-1
+    # https://github.com/mapzen/wiki/wiki/mapzen.com-OAuth
     #
-    data = dict(client_id=current_app.config['GITHUB_CLIENT_ID'],
-                client_secret=current_app.config['GITHUB_CLIENT_SECRET'],
+    data = dict(client_id=current_app.config['MAPZEN_APP_ID'],
+                client_secret=current_app.config['MAPZEN_APP_SECRET'],
                 redirect_uri=urljoin(request.url, '/oauth/callback'),
                 code=code, grant_type='authorization_code')
 
-    resp = post('https://mapzen.com/oauth/token', urlencode(data))
+    resp = post(mapzen_token_url, urlencode(data))
     auth = resp.json()
     
     if 'error' in auth:
-        return render_template('error-oauth.html', reason='Github said "%(error)s".' % auth)
+        return render_template('error-oauth.html', reason='Mapzen said "%(error)s".' % auth)
     
     elif 'access_token' not in auth:
         return render_template('error-oauth.html', reason="missing `access_token`.")
@@ -165,8 +169,9 @@ def get_oauth_callback():
     #
     # Figure out who's here.
     #
-    url = 'https://mapzen.com/developers/oauth_api/current_developer'
-    _ = OAuth2Session(current_app.config['GITHUB_CLIENT_ID'], token=session['token']).get(url).json()
+    head = {'Authorization': 'Bearer {}'.format(session['token']['access_token'])}
+
+    _ = get(mapzen_currdev_url, headers=head).json()
     id = dict(id=_['id'], email=_['email'], nickname=_['nickname'], keys_url=_['keys'])
     session['id'] = id
     
