997fa9a14865df39042940faab69cfce31c57442
Michal Migurski <mike@teczno.com>
2016-06-27 17:49:10 -0700

    Added display for previously-existing extracts

--- App/odes.py
+++ App/odes.py
@@ -1,11 +1,39 @@
-from flask import Blueprint, url_for
+from operator import itemgetter
+
+from flask import Blueprint, url_for, session, render_template, jsonify
+
+import requests
+import uritemplate
 
 blueprint = Blueprint('ODES', __name__, template_folder='templates/odes')
+
+odes_extracts_url = 'https://odes.mapzen.com/extracts{/id}{?api_key}'
 
 def apply_odes_blueprint(app):
     '''
     '''
     app.register_blueprint(blueprint)
+
+def get_odes_key(keys_url, access_token):
+    auth_header = 'Bearer {}'.format(access_token)
+
+    resp = requests.get(keys_url, headers={'Authorization': auth_header})
+    keys = sorted(resp.json(), key=itemgetter('created_at'), reverse=True)
+    api_key = [key['key'] for key in keys if key['service'] == 'odes'][0]
+    
+    return api_key
+
+def load_extracts(api_key):
+    '''
+    '''
+    extracts_url = uritemplate.expand(odes_extracts_url, dict(api_key=api_key))
+    return list(requests.get(extracts_url).json())
+
+def load_extract(api_key, id):
+    '''
+    '''
+    extract_url = uritemplate.expand(odes_extracts_url, dict(id=id, api_key=api_key))
+    return dict(requests.get(extract_url).json())
 
 @blueprint.route('/odes')
 def get_odes():
@@ -20,10 +48,28 @@ def get_odes():
             <label><input value="-122.24825" name="bbox_e"> East</label><br>
             <input type="submit">
         </form>
-        '''.format(href=url_for('.post_extract'))
+        '''.format(href=url_for('.post_extracts'))
 
 @blueprint.route('/odes/extracts', methods=['POST'])
-def post_extract():
+def post_extracts():
     '''
     '''
-    return 'We will get right on that.'
+    return str(session) # 'We will get right on that.'
+
+@blueprint.route('/odes/extracts', methods=['GET'])
+def get_extracts():
+    '''
+    '''
+    api_key = get_odes_key(session['id']['keys_url'], session['token']['access_token'])
+    extracts = load_extracts(api_key)
+
+    return render_template('extracts.html', extracts=extracts)
+
+@blueprint.route('/odes/extracts/<extract_id>', methods=['GET'])
+def get_extract(extract_id):
+    '''
+    '''
+    api_key = get_odes_key(session['id']['keys_url'], session['token']['access_token'])
+    extract = load_extract(api_key, extract_id)
+
+    return render_template('extract.html', extract=extract)
