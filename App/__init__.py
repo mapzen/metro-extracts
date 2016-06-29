9a765c57db644541285a17d8f5d228a249456abe
Michal Migurski <mike@teczno.com>
2016-06-28 17:20:13 -0700

    Switched to GeoJSON cities data generated internally

--- App/__init__.py
+++ App/__init__.py
@@ -1,12 +1,23 @@
 from flask import Blueprint, jsonify, Response, render_template
 from itertools import groupby
 from operator import itemgetter
+from os.path import join, dirname
 import json
 
 import requests
 import uritemplate
 
 blueprint = Blueprint('Metro-Extracts', __name__)
+
+def load_cities(filename):
+    '''
+    '''
+    with open(filename) as file:
+        cities = json.load(file)
+    
+    return cities
+
+cities = load_cities(join(dirname(__file__), '..', 'cities.json'))
 
 def apply_blueprint(app):
     '''
@@ -15,9 +26,6 @@ def apply_blueprint(app):
 
 @blueprint.route('/')
 def index():
-    with open('cities.json') as file:
-        cities = json.load(file)
-    
     ordered_cities = sorted(cities, key=itemgetter('country'))
     metros_tree = list()
     
@@ -28,6 +36,23 @@ def index():
             })
     
     return render_template('index.html', metros_tree=metros_tree)
+
+@blueprint.route('/cities.geojson')
+def get_cities_geojson():
+    features = list()
+    
+    for city in cities:
+        x1, y1, x2, y2 = [float(city['bbox'][k])
+                          for k in ('left', 'bottom', 'right', 'top')]
+
+        feature = dict(type='Feature', id=city['name'])
+        #feature['bbox'] = [x1, y1, x2, y2]
+        feature['geometry'] = dict(type='Polygon')
+        feature['geometry']['coordinates'] = [[[x1, y1], [x1, y2], [x2, y2], [x2, y1], [x1, y1]]]
+        feature['properties'] = dict(name=city['name'])
+        features.append(feature)
+
+    return jsonify(dict(features=features))
 
 @blueprint.route('/metro/<name>')
 def metro(name):
