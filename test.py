4759a919739a0765f2da7bcb5d6dff28c9424c6b
Michal Migurski <mike@teczno.com>
2016-06-29 15:44:42 -0700

    Added BeautifulSoup to step through HTML responses in tests

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import unittest
 from uuid import uuid4
 
 from App.web import make_app
+from bs4 import BeautifulSoup
 from flask import Flask
 
 app = Flask(__name__)
@@ -23,13 +24,28 @@ class TestApp (unittest.TestCase):
         self.client = app.test_client()
 
     def test_index(self):
-        resp = self.client.get(self.prefixed('/'))
-        self.assertEqual(resp.status_code, 200)
+        resp1 = self.client.get(self.prefixed('/'))
+        soup1 = BeautifulSoup(resp1.data, 'html.parser')
+        head1 = soup1.find('h1').text
 
+        self.assertEqual(resp1.status_code, 200)
+        self.assertIn('metro extracts', head1)
+        
+        link1 = soup1.find_all(text='San Francisco')[0].find_parent('a')
+        resp2 = self.client.get(link1['href'])
+        soup2 = BeautifulSoup(resp2.data, 'html.parser')
+        head2 = soup2.find('h1').text
+
+        self.assertEqual(resp2.status_code, 200)
+        self.assertIn('San Francisco', head2)
+        
     def test_oauth_index(self):
         resp = self.client.get(self.prefixed('/oauth/hello'))
+        soup = BeautifulSoup(resp.data, 'html.parser')
+        head = soup.find('h2').text
+
         self.assertEqual(resp.status_code, 401)
-        self.assertIn(b'Authenticate With Mapzen', resp.data)
+        self.assertIn('Authenticate With Mapzen', head)
 
     def test_odes_index(self):
         resp = self.client.get(self.prefixed('/odes/'))
