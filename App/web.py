19e5eb62930e310e17246d85c2abae1e12d356f5
Michal Migurski <mike@teczno.com>
2016-06-29 15:28:44 -0700

    Added and tested optional URL prefix for main app blueprint

--- App/web.py
+++ App/web.py
@@ -4,12 +4,12 @@ from .oauth import apply_oauth_blueprint
 from .odes import apply_odes_blueprint
 from flask import Flask
 
-def make_app():
+def make_app(url_prefix):
     app = Flask(__name__)
-    apply_blueprint(app)
+    apply_blueprint(app, url_prefix)
     apply_oauth_blueprint(app)
     apply_odes_blueprint(app)
 
     return app
 
-app = make_app()
+app = make_app(environ.get('URL_PREFIX'))
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import unittest
+from uuid import uuid4
 
 from App.web import make_app
 from flask import Flask
@@ -7,14 +8,22 @@ from flask import Flask
 app = Flask(__name__)
 
 class TestApp (unittest.TestCase):
+
+    _url_prefix = None
+    
+    def prefixed(self, path):
+        return ''.join((self._url_prefix or '', path))
     
     def setUp(self):
-        app = make_app()
+        app = make_app(self._url_prefix)
         self.client = app.test_client()
 
     def test_index(self):
-        resp = self.client.get('/')
+        resp = self.client.get(self.prefixed('/'))
         self.assertEqual(resp.status_code, 200)
+
+class TestAppPrefix (TestApp):
+    _url_prefix = '/{}'.format(uuid4())
 
 if __name__ == '__main__':
     unittest.main()
