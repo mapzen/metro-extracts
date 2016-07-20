2443da55add55547d4f60d173348db5d6acd789d
binx <rachelbinx@gmail.com>
2016-07-20 10:11:22 -0700

    implementing hanbyuls fallbacktile

--- App/static/js/extract.js
+++ App/static/js/extract.js
@@ -9,14 +9,6 @@ var Extract = function (){
       this.initDisplayMap();
       return this;
     },
-    hasWebGL: function() {
-      try {
-        var canvas = document.createElement('canvas')
-        return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')))
-      } catch (x) {
-        return false
-      }
-    },
     initDisplayMap: function() {
       var southwest = L.latLng(bbox.s, bbox.w),
         northeast = L.latLng(bbox.n, bbox.e),
@@ -27,15 +19,11 @@ var Extract = function (){
           dragging: false,
           tap: false,
           scene: sceneURL,
-          attribution: '<a href="https://mapzen.com/tangram">Tangram</a> | <a href="http://www.openstreetmap.org/copyright">&copy; OSM contributors</a> | <a href="https://mapzen.com/">Mapzen</a>'
+          attribution: '<a href="https://mapzen.com/tangram">Tangram</a> | <a href="http://www.openstreetmap.org/copyright">&copy; OSM contributors</a> | <a href="https://mapzen.com/">Mapzen</a>',
+          fallbackTile: L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'})
         };
       displayMap = L.Mapzen.map('map', options).fitBounds(L.latLngBounds(southwest, northeast)).zoomOut(1);
-
-      if(!this.hasWebGL()) {
-        L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
-          attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
-        }).addTo(map);
-      }
 
       var rect = new L.Rectangle(new L.LatLngBounds([southwest, northeast]), { className : "blue" });
       displayMap.addLayer(rect);
--- App/static/js/metro.js
+++ App/static/js/metro.js
@@ -10,14 +10,6 @@ var Metro = function (){
       this.initDisplayMap();
       return this;
     },
-    hasWebGL : function() {
-      try {
-        var canvas = document.createElement('canvas')
-        return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')))
-      } catch (x) {
-        return false
-      }
-    },
     initDisplayMap : function() {
       var southwest = L.latLng(metro.bbox.bottom, metro.bbox.right),
         northeast = L.latLng(metro.bbox.top, metro.bbox.left),
@@ -28,15 +20,11 @@ var Metro = function (){
           dragging: false,
           tap: false,
           scene: sceneURL,
-          attribution: '<a href="https://mapzen.com/tangram">Tangram</a> | <a href="http://www.openstreetmap.org/copyright">&copy; OSM contributors</a> | <a href="https://mapzen.com/">Mapzen</a>'
+          attribution: '<a href="https://mapzen.com/tangram">Tangram</a> | <a href="http://www.openstreetmap.org/copyright">&copy; OSM contributors</a> | <a href="https://mapzen.com/">Mapzen</a>',
+          fallbackTile: L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'})
         };
       displayMap = L.Mapzen.map('map', options).fitBounds(L.latLngBounds(southwest, northeast)).zoomOut(1);
-
-      if(!this.hasWebGL()) {
-        L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
-          attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
-        }).addTo(map);
-      }
 
       var rect = new L.Rectangle(new L.LatLngBounds([southwest, northeast]));
       displayMap.addLayer(rect);
