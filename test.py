5fab4afde861fd91e92309f5b62975e4745b5261
Michal Migurski <mike@teczno.com>
2016-08-01 16:21:43 -0700

    Added failing display name arguments to data tests

--- test.py
+++ test.py
@@ -454,33 +454,34 @@ class TestAppDoublePrefix (TestApp):
 class TestData (unittest.TestCase):
 
     def test_add_extract_envelope(self):
-        db = Mock()
+        db, name = Mock(), str(uuid4())
         envelope = data.Envelope('xyz', [-122.26447, 37.79724, -122.24825, 37.81230])
         wof = data.WoF(85921881, 'Oakland')
-        extract_id = data.add_extract_envelope(db, envelope, wof)
+        extract_id = data.add_extract_envelope(db, name, envelope, wof)
         
         self.assertEqual(db.mock_calls[0][0], 'execute')
-        self.assertEqual(db.mock_calls[0][1][1], (extract_id, envelope.id, envelope.bbox, wof.name, wof.id))
+        self.assertEqual(db.mock_calls[0][1][1], (extract_id, name, envelope.id, envelope.bbox, wof.name, wof.id))
         self.assertEqual(db.mock_calls[0][1][0], '''
         INSERT INTO extracts
-        (id, envelope_id, envelope_bbox, wof_name, wof_id, created)
-        VALUES (%s, %s, %s, %s, %s, NOW())
+        (id, name, envelope_id, envelope_bbox, wof_name, wof_id, created)
+        VALUES (%s, %s, %s, %s, %s, %s, NOW())
         ''')
     
     def test_get_extract_by_id(self):
         db = Mock()
-        db.fetchone.return_value = ('123', '456', [1,2,3,4], '7', 8, 'Oakland', 85921881, None)
+        db.fetchone.return_value = ('123', 'XYZ', '456', [1,2,3,4], '7', 8, 'Oakland', 85921881, None)
 
         extract = data.get_extract(db, extract_id='123')
         
         self.assertEqual(db.mock_calls[0][0], 'execute')
         self.assertEqual(db.mock_calls[0][1][1], ('123', ))
         self.assertEqual(db.mock_calls[0][1][0], '''
-        SELECT id, envelope_id, envelope_bbox, odes_id, user_id, wof_name, wof_id, created
+        SELECT id, name, envelope_id, envelope_bbox, odes_id, user_id, wof_name, wof_id, created
         FROM extracts WHERE id = %s
         ''')
         
         self.assertEqual(extract.id, '123')
+        self.assertEqual(extract.name, 'XYZ')
         self.assertEqual(extract.odes.id, '7')
         self.assertEqual(extract.user_id, 8)
         self.assertEqual(extract.envelope.id, '456')
@@ -490,18 +491,19 @@ class TestData (unittest.TestCase):
 
     def test_get_extract_by_envelope(self):
         db = Mock()
-        db.fetchone.return_value = ('123', '456', [1,2,3,4], '7', 8, 'Oakland', 85921881, None)
+        db.fetchone.return_value = ('123', 'XYZ', '456', [1,2,3,4], '7', 8, 'Oakland', 85921881, None)
 
         extract = data.get_extract(db, envelope_id='456')
         
         self.assertEqual(db.mock_calls[0][0], 'execute')
         self.assertEqual(db.mock_calls[0][1][1], ('456', ))
         self.assertEqual(db.mock_calls[0][1][0], '''
-        SELECT id, envelope_id, envelope_bbox, odes_id, user_id, wof_name, wof_id, created
+        SELECT id, name, envelope_id, envelope_bbox, odes_id, user_id, wof_name, wof_id, created
         FROM extracts WHERE envelope_id = %s
         ''')
         
         self.assertEqual(extract.id, '123')
+        self.assertEqual(extract.name, 'XYZ')
         self.assertEqual(extract.odes.id, '7')
         self.assertEqual(extract.user_id, 8)
         self.assertEqual(extract.envelope.id, '456')
@@ -511,7 +513,7 @@ class TestData (unittest.TestCase):
 
     def test_get_extract_by_odes(self):
         db = Mock()
-        db.fetchone.return_value = ('123', '456', [1,2,3,4], '7', 8, 'Oakland', 85921881, None)
+        db.fetchone.return_value = ('123', 'XYZ', '456', [1,2,3,4], '7', 8, 'Oakland', 85921881, None)
         
         odes = data.ODES('7')
         extract = data.get_extract(db, odes=odes)
@@ -519,11 +521,12 @@ class TestData (unittest.TestCase):
         self.assertEqual(db.mock_calls[0][0], 'execute')
         self.assertEqual(db.mock_calls[0][1][1], ('7', ))
         self.assertEqual(db.mock_calls[0][1][0], '''
-        SELECT id, envelope_id, envelope_bbox, odes_id, user_id, wof_name, wof_id, created
+        SELECT id, name, envelope_id, envelope_bbox, odes_id, user_id, wof_name, wof_id, created
         FROM extracts WHERE odes_id = %s
         ''')
         
         self.assertEqual(extract.id, '123')
+        self.assertEqual(extract.name, 'XYZ')
         self.assertEqual(id(extract.odes), id(odes))
         self.assertEqual(extract.user_id, 8)
         self.assertEqual(extract.envelope.id, '456')
@@ -535,18 +538,18 @@ class TestData (unittest.TestCase):
             odes2 = data.ODES(7)
 
     def test_set_extract(self):
-        db = Mock()
+        db, name = Mock(), str(uuid4())
         envelope = data.Envelope('xyz', [-122.26447, 37.79724, -122.24825, 37.81230])
         wof = data.WoF(85921881, 'Oakland')
         odes = data.ODES('4')
-        extract = data.Extract('123', envelope, odes, 5, None, wof)
+        extract = data.Extract('123', name, envelope, odes, 5, None, wof)
         data.set_extract(db, extract)
         
         self.assertEqual(db.mock_calls[0][0], 'execute')
-        self.assertEqual(db.mock_calls[0][1][1], ('xyz', [-122.26447, 37.79724, -122.24825, 37.8123], '4', 5, 'Oakland', 85921881, '123'))
+        self.assertEqual(db.mock_calls[0][1][1], (name, 'xyz', [-122.26447, 37.79724, -122.24825, 37.8123], '4', 5, 'Oakland', 85921881, '123'))
         self.assertEqual(db.mock_calls[0][1][0], '''
         UPDATE extracts
-        SET envelope_id = %s, envelope_bbox = %s, odes_id = %s,
+        SET name = %s, envelope_id = %s, envelope_bbox = %s, odes_id = %s,
             user_id = %s, wof_name = %s, wof_id = %s
         WHERE id = %s
         ''')
